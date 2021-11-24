ba7dbf093dfc297b5c30ce729c5672dfacd6203f
rajiv pratap singh <rajivpratapsingh1999@gmail.com>
2021-11-24 13:22:30 +0530

    after changing for 1st tme

--- script.js
+++ script.js
@@ -1,34 +1,90 @@
-// function compute()
-// {
-//     p = document.getElementById("principal").value;
-    
-// }
-        
+const formulario = document.querySelector(".formulario");
+const result = document.getElementById("result");
+const again = document.getElementById("again");
+const amount = document.getElementById("amount");
+const rate = document.getElementById("rate");
+const years = document.getElementById("years");
 
-/* fonction pour récupérer les valeurs aléatoires du rate*/
-function rateValue(val){
-	document.getElementById('rateInput').value=val;
+function updateRateValue(val) {
+  let rate = document.getElementById("rate");
+  let output = document.getElementById("output_rate");
 
+  output.innerHTML = `${val}%`;
+  console.log(rate.value);
 }
 
-/* fonction de calcul de l'intérêt*/
-function calculate(){
-	/* déclaration des variables*/
-amount = document.getElementById('amount').value;
-rateInput= document.getElementById('rateInput').value;
-selectId=document.getElementById('selectId').value;
-resultat=document.getElementById('resultat');
+function compute(event) {
+  event.preventDefault();
 
-/* calcul du nombre d'année*/
-var today= new Date();
-var annee = today.getFullYear();
-var result1=((annee=+selectId));
-var today2=new Date();
-var annee2 =today2.getFullYear();
-var result2=result1+annee2
+  if (
+    amount.value &&
+    amount.value > 0 &&
+    rate.value &&
+    rate.value <= 100 &&
+    years.value &&
+    years.value > 0
+  ) {
+    let date = new Date();
 
-/* calul et affichage de l'intérêt*/
-resultat.innerHTML="If you deposit"+"&nbsp;"+(amount)+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+(rateInput)+"."+"<br/>"+
-"You will receive an amount of"+"&nbsp;"+(amount*rateInput*selectId/100)+","+"<br/>"+
-"in the year"+"&nbsp;"+(result2);
+    result.innerHTML = `If you deposit <span class="span_yellow">${
+      "$" + amount.value
+    }</span>, <br>
+                            at an interest rate of <span class="span_yellow">${
+                              rate.value
+                            }%</span>, <br>
+                            You will receive an amount of <span class="span_yellow">${
+                              "$" +
+                              parseInt(years.value) *
+                                parseInt(amount.value) *
+                                (parseFloat(rate.value) / 100)
+                            }</span>, <br>
+                            in the year <span class="span_yellow">${
+                              date.getFullYear() + parseInt(years.value)
+                            }</span>
+                            `;
+
+    document.getElementById("label_amount").style.color = "black";
+    document.getElementById("label_years").style.color = "black";
+    document.getElementById("label_rate").style.color = "black";
+  } else {
+    if (!(amount.value && amount.value > 0)) {
+      document.getElementById("label_amount").style.color = "red";
+      if (amount.value <= 0) {
+        alert("You must enter a positive number greater than 0");
+      }
+      amount.focus();
+    } else {
+      document.getElementById("label_amount").style.color = "black";
+    }
+
+    if (!(years.value && years.value > 0)) {
+      document.getElementById("label_years").style.color = "red";
+      years.focus();
+      if (years.value <= 0) {
+        alert("You must enter a positive year");
+      }
+    } else {
+      document.getElementById("label_years").style.color = "black";
+    }
+
+    if (!rate.value && rate.value > 100) {
+      document.getElementById("label_rate").style.color = "red";
+      rate.focus();
+    } else {
+      document.getElementById("label_rate").style.color = "black";
+    }
+  }
 }
+
+formulario.addEventListener("submit", compute);
+
+again.addEventListener("click", () => {
+  amount.value = "";
+  rate.value = "";
+  years.value = "";
+  amount.focus();
+  result.innerHTML = "";
+  document.getElementById("output_rate").innerHTML = "10%";
+});
+
+//updateRateValue(val)
